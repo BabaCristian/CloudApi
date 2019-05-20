4d21b43b04c9aeb9e7db4401bf19edd7f6e7297a
Unknown <cristian.baba09@yahoo.com>
2019-05-20 16:27:16 +0200

    Working on Backend (Api)

---
+++ CristianBabaApi/CristianBabaApi/Models/Whisky.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CristianBabaApi.Models
+{
+    public class Whisky
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        // [required ] en [stringlength(100)]           required dient dat het een verplichte veld is en length is lengte van u data
+        public string Name { get; set; }
+
+    }
+}
